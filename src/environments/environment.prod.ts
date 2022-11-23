cf4283edf22ae6024b24dcd9feacc406dc3986db
Laurens256 <duinlaurens2@gmail.com>
2022-11-23 10:06:25 +0100

    change env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   client_id: '176a1a3e2d5a4065983995129a9b96bf',
-  apiUrl: 'http://localhost:3000/api/authorize/'
+  apiUrl: 'https://tech-track-22-23.vercel.app/api/authorize/'
 };
